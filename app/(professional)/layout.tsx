5452abfb4ca3a0d978dc7eb8be39051cbc9a6b7a
Curtis197 <curtiscapre@gmail.com>
2026-04-06 22:34:15 +0200

    fix: remove public navbar and footer from professional dashboard

--- app/(professional)/layout.tsx
+++ app/(professional)/layout.tsx
@@ -1,6 +1,4 @@
 import { ProSidebar } from "@/components/layout/ProSidebar";
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 
 export default function ProfessionalLayout({
   children,
@@ -9,14 +7,12 @@ export default function ProfessionalLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar />
       <div className="flex flex-1">
         <ProSidebar />
-        <main className="flex-1 bg-muted/30">
+        <main className="flex-1 bg-surface">
           <div className="p-4 pb-24 lg:p-8 lg:pb-8">{children}</div>
         </main>
       </div>
-      <div className="hidden lg:block"><Footer /></div>
     </div>
   );
 }
